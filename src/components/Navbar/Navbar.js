7328beaba7348f088dfa0b5cbed15d2521d2badd
sylvernus akubo <akubo.sylvernus@raaxo.ng>
2021-05-18 23:11:39 +0100

    sidebar header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -139,14 +139,6 @@ export default function Navbar() {
                 <span className="title">SDKs &amp; Plugins</span>{" "}
                 <span className="arrow down" />
               </button>{" "}
-              <button
-                type="button"
-                aria-label="SDKs & Plugins"
-                className="mobile-dropdown-title"
-              >
-                <span className="title">SDKs &amp; Plugins</span>{" "}
-                <span className="arrow right" />
-              </button>{" "}
               <ul className="nav-dropdown" style={{ display: "none" }}>
                 <li className="dropdown-item">
                   {/**/}{" "}
@@ -172,27 +164,7 @@ export default function Navbar() {
             >
               FAQs
               <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                  focusable="false"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 100 100"
-                  width={15}
-                  height={15}
-                  className="icon outbound"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-                  />{" "}
-                  <polygon
-                    fill="currentColor"
-                    points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-                  />
-                </svg>{" "}
-                <span className="sr-only">(opens new window)</span>
+                <OpenNewWindow />
               </span>
             </a>
           </div>
@@ -206,7 +178,6 @@ export default function Navbar() {
               Contact Us
               <span>
                 <OpenNewWindow />
-                <span className="sr-only">(opens new window)</span>
               </span>
             </a>
           </div>{" "}
